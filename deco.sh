a14a2a640d8309ba67b360812e9758ab1e421083
Emilio Herrera <emhege@floridauniversitaria.es>
2023-02-16 09:54:43 +0100

    deco

---
+++ deco.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+param=$1
+contador=0
+totalConsumo=0
+mediaConsumo=0
+while read -r line
+do
+    consumo=$(echo "$line" | cut -d ' ' -f 4)
+    ciudad=$(echo "$line" | cut -d ' ' -f 1)
+   
+    if [ $ciudad == $1 ]
+    then
+        ((contador++))
+        totalConsumo=$((totalConsumo+consumo))
+    fi
+done < consumos.txt
+mediaConsumo=$((totalConsumo/contador))
+
+echo "El consumo medio fue de $mediaConsumo"
+if [ $mediaConsumo -lt 400 ]
+then
+    echo "ECO"
+else
+    echo "No ECO"
+fi
